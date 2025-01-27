cf50fa2cbb0c58f7eca7e3e42ff2e6480c3659da
lavkushsingh1998 <lavkushsingh1998@gmail.com>
2025-01-27 09:11:33 +0000

    upadted by gemini

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-// import Services from './pages/Services'
-// import About from './pages/About'
-// import Contact from './pages/Contact'
-// import SignUp from './pages/SignUp'
+import Services from './pages/Services'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import SignUp from './pages/SignUp'
 import './App.css'
 
 function App() {
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 function Home() {
-    return (
-      <div className="page">
-        <h1>Welcome to Globe Printers</h1>
-        {/* Add your home page content here */}
+  return (
+    <div className="page">
+      <h1>Welcome to Globe Printers</h1>
+        <div className="home-content">
+          <div>
+            <h2>High-Quality Printing</h2>
+          </div>
+          <div>
+            <h2>Custom Design Solutions</h2>
+          </div>
+          <div>
+            <h2>Eco-Friendly Options</h2>
+          </div>
       </div>
-    )
-  }
-  
-  export default Home
+    </div>
+  );
+}
+
+export default Home;
